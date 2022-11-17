c807a9082769fe7be6829fbafad5d7c07b40bf50
Dmitry Vasilev <dmitry@t3dev.ru>
2022-11-17 22:43:55 +0300

    Add firebase_subscribe.js

---
+++ public_html/firebase-messaging-sw.js
@@ -0,0 +1,9 @@
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
+
+firebase.initializeApp({
+    messagingSenderId: '925306306254'
+});
+
+const messaging = firebase.messaging();
