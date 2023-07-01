b0857cc99cd289deb504dc776b47aeff9d9bcee0
Dmitry Vasilev <dmitry@t3dev.ru>
2023-07-01 18:20:05 +0300

    [TASK #83] - redirect if performer balance not enough

--- src/Service/Order/SetBalanceService.php
+++ src/Service/Order/SetBalanceService.php
@@ -5,6 +5,11 @@ namespace App\Service\Order;
 use App\Repository\ProjectRepository;
 use App\Repository\UserRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Notifier\Notification\Notification;
+use Symfony\Component\Notifier\NotifierInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SetBalanceService
 {
@@ -19,19 +24,33 @@ class SetBalanceService
         GetTaxService $getTaxService,
         ManagerRegistry $doctrine,
         ProjectRepository $projectRepository,
-        int $projectId
+        int $projectId,
+        TranslatorInterface $translator,
+        NotifierInterface $notifier,
+        RouterInterface $router
     ) {
         $this->userRepository = $userRepository;
         $this->getTaxService = $getTaxService;
         $this->doctrine = $doctrine;
         $this->projectRepository = $projectRepository;
         $this->projectId = $projectId;
+        $this->translator = $translator;
+        $this->notifier = $notifier;
+        $this->router = $router;
     }
 
     public function setBalance($order)
     {
         $performer = $order->getPerformer();
         $tax = $this->getTaxService->getTax($order);
+        $orderTaxRate = $order->getCustomTaxRate();
+
+        // Redirect if performer balance not enought
+        if ($performer->getBalance() <= ($tax + $orderTaxRate)) {
+            $message = $this->translator->trans('Not enought balance', array(), 'flash');
+            $this->notifier->send(new Notification($message, ['browser']));
+            return new RedirectResponse($this->router->generate('app_top_up_balance'));
+        }
 
         // If order created by client
         if ($order->getTypeCreated() == self::CREATED_BY_CLIENT) {
@@ -40,9 +59,7 @@ class SetBalanceService
 
         // If order created by company
         if ($order->getTypeCreated() == self::CREATED_BY_COMPANY) {
-            $orderTaxRate = $order->getCustomTaxRate();
             $newMasterBalance = $performer->getBalance() - $tax - $orderTaxRate;
-
             $company = $this->userRepository->findOneBy(['id' => $order->getUsers()->getId()]);
             $currentCompanyBalance = (float)$company->getBalance();
             $newCompanyBalance = $currentCompanyBalance + $orderTaxRate;
@@ -58,5 +75,7 @@ class SetBalanceService
 
         $entityManager = $this->doctrine->getManager();
         $entityManager->flush();
+
+        return 0;
     }
 }
